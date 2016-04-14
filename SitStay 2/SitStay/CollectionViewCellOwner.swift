572629a4dd06ced2358160d80b34f3222f31e4e5
M3gster <mmc2n8@mail.missouri.edu>
2016-04-14 12:40:58 -0500

    going to revert this

---
+++ SitStay 2/SitStay/CollectionViewCellOwner.swift
@@ -0,0 +1,18 @@
+//
+//  Owner_CollectionViewCell.swift
+//  Pet_Tab
+//
+//  Created by MU IT Program on 3/30/16.
+//  Copyright © 2016 Megan Cochran. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCellOwner: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var petButton: UIButton!
+    
+    
+    
+}
