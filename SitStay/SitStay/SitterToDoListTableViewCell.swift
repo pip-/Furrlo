b03c04cff3e0e53e700f9fcb4202aa25aca21f81
jgpc65 <muitprogram@MUs-MBP-5.centurylink.net>
2016-04-19 20:29:33 -0500

    Added two View Controllers
    added SitterToDoListTableViewCell and SitterToDoListTableViewController

---
+++ SitStay/SitStay/SitterToDoListTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  SitterToDoListTableViewCell.swift
+//  SitStay
+//
+//  Created by MU IT Program on 4/19/16.
+//  Copyright © 2016 GroupA. All rights reserved.
+//
+
+import UIKit
+
+class SitterToDoListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
