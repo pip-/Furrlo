d483087da511979b2d2c29a0a85e615303d0ac41
Philip Gilbreth <pgilbreth@gmail.com>
2016-04-26 20:14:01 -0500

    Added a few object attributes to Pet

--- SitStay/SitStay/Pet+CoreDataProperties.swift
+++ SitStay/SitStay/Pet+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Pet+CoreDataProperties.swift
 //  SitStay
 //
-//  Created by Philip Gilbreth on 4/24/16.
+//  Created by Philip Gilbreth on 4/26/16.
 //  Copyright © 2016 GroupA. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -20,6 +20,7 @@ extension Pet {
     @NSManaged var notes: String?
     @NSManaged var personality: String?
     @NSManaged var petID: NSNumber?
+    @NSManaged var type: String?
     @NSManaged var species: String?
     @NSManaged var food: String?
     @NSManaged var tripID: Trip?
