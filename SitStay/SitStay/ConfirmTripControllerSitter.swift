e0508924da11aeda95299a92a1a397dc3e5945e8
Philip Gilbreth <pgilbreth@gmail.com>
2016-04-28 23:27:17 -0500

    Now Pulls Trips on Trip Confirm

--- SitStay/SitStay/ConfirmTripControllerSitter.swift
+++ SitStay/SitStay/ConfirmTripControllerSitter.swift
@@ -12,6 +12,7 @@ class ConfirmTripControllerSitter: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
     var values:NSArray = []
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
     override func viewDidLoad() {
         super.viewDidLoad()
     
@@ -27,7 +28,6 @@ class ConfirmTripControllerSitter: UIViewController {
     @IBAction func submit(sender: AnyObject) {
         
         //ToDo, pull trip object where trip.tripID = textField.text and all associated pets and to-do lists
-        //let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         //let fetchedPets = appDelegate.getPets()
         
         //
@@ -65,8 +65,60 @@ class ConfirmTripControllerSitter: UIViewController {
             
             print("response = \(response)")
             
-            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            var responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
             print("responseString = \(responseString)")
+            
+            //Strip Escape Characters-------------------
+            responseString = responseString?.stringByReplacingOccurrencesOfString("\n", withString: "")
+            responseString = responseString?.stringByReplacingOccurrencesOfString("\r", withString: "")
+            //------------------------------------------
+            
+            //Change to easier delimiters---------------
+            responseString = responseString?.stringByReplacingOccurrencesOfString("},{", withString: "}&{")
+            //------------------------------------------
+            if let responseString = responseString{
+                //Convert to String/Drop Garbage------------
+                let s = String(responseString)
+                var parsedJsonString = String(s.characters.dropLast(147))
+                parsedJsonString = String(parsedJsonString.characters.dropFirst())
+                //------------------------------------------
+                
+                
+                //Put into array----------------------------
+                let tripStrings: [String] = parsedJsonString.characters.split("&").map(String.init)
+                //------------------------------------------
+                
+                //Parse each string into dictionary---------
+                var tripDicts: [[String: String]] = []
+                for string in tripStrings{
+                    if let dict = string.convertToDictionary(){
+                        tripDicts.append(dict)
+                    }
+                }
+                //-------------------------------------------
+                
+                //Insert the new trip----------------------
+                let dateFormatter = NSDateFormatter()
+                dateFormatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
+                for dict in tripDicts{
+                    print(dict)
+                    if let startDate =
+                        dict["TripStartDate"]?.toDateTime() {
+                        if let endDate = dict["TripEndDate"]?.toDateTime(){
+                    if let addr2 = dict["UserAddress2"]{
+                        self.appDelegate.insertNewTrip(startDate, endDate: endDate, street: dict["UserAddress"]!, zip: dict["Zipcode"]!, city: dict["City"]!, addr2: addr2, pets: [], tripName: dict["tripName"]!, isSitting: true)
+                    }
+                    else {
+                        self.appDelegate.insertNewTrip(startDate, endDate: endDate, street: dict["UserAddress"]!, zip: dict["Zipcode"]!, city: dict["City"]!, addr2: nil, pets: [], tripName: dict["tripName"]!, isSitting: true)
+                    }
+                        }
+                    } else {
+                        print("Wrong format")
+                    }
+                }
+                //-------------------------------------------
+            }
+
             
         }
         task.resume()
@@ -84,4 +136,25 @@ class ConfirmTripControllerSitter: UIViewController {
     }
     */
 
+}
+
+extension String
+{
+    func toDateTime() -> NSDate
+    {
+        //Create Date Formatter
+        let dateFormatter = NSDateFormatter()
+        
+        //dateFormatter.locale = NSLocale(localeIdentifier: "us")
+        
+        //Specify Format of String to Parse
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        //Parse into NSDate
+        let dateFromString : NSDate = dateFormatter.dateFromString(self)!
+        
+        
+        //Return Parsed Date
+        return dateFromString
+    }
 }
