fad16c3635a7113d16c8419ae50e03e9228b0c12
mmclaughlin557 <mcm6y9@mail.missouri.edu>
2016-05-01 17:39:30 -0500

    Redid some stuff Owner add task

--- SitStay/SitStay/OwnerAddTaskViewController.swift
+++ SitStay/SitStay/OwnerAddTaskViewController.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 
-class OwnerAddTaskViewController: UIViewController {
+class OwnerAddTaskViewController: UIViewController, UIPickerViewDataSource,UIPickerViewDelegate {
 
+    @IBOutlet weak var taskPicker: UIPickerView!
+    @IBOutlet weak var selectionLabel: UILabel!
+    
+     var tasks : [String] = ["Food","Water","Exercise","Medicine","Other"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        taskPicker.dataSource = self
+        taskPicker.delegate = self
+    
 
         // Do any additional setup after loading the view.
     }
@@ -21,6 +30,21 @@ class OwnerAddTaskViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return tasks.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return tasks[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectionLabel.text = tasks[row]
+    }
 
     /*
     // MARK: - Navigation
