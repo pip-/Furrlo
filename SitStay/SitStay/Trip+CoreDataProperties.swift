b5939170c97118466576a58e5652edd9c495810a
Philip Gilbreth <pgilbreth@gmail.com>
2016-05-02 19:29:34 -0500

    Revert "Merge branch 'master' into michaels-branch-2"
    This reverts commit b94525f36ede4e1936a5585880e9f9c45e4e7aea, reversing
    changes made to a407157405d22a42356fa91f2166d25366cbd0ec.

--- SitStay/SitStay/Trip+CoreDataProperties.swift
+++ SitStay/SitStay/Trip+CoreDataProperties.swift
@@ -2,11 +2,7 @@
 //  Trip+CoreDataProperties.swift
 //  SitStay
 //
-<<<<<<< HEAD
 //  Created by Philip Gilbreth on 4/30/16.
-=======
-//  Created by Philip Gilbreth on 4/24/16.
->>>>>>> parent of 1000481... Trying to get Owner to work
 //  Copyright © 2016 GroupA. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -22,19 +18,13 @@ extension Trip {
     @NSManaged var addr2: String?
     @NSManaged var city: String?
     @NSManaged var endDate: NSDate?
-<<<<<<< HEAD
     @NSManaged var isSitting: NSNumber?
-=======
->>>>>>> parent of 1000481... Trying to get Owner to work
     @NSManaged var startDate: NSDate?
     @NSManaged var tripID: NSNumber?
     @NSManaged var tripName: String?
     @NSManaged var zip: String?
-<<<<<<< HEAD
     @NSManaged var phone: String?
     @NSManaged var email: String?
-=======
->>>>>>> parent of 1000481... Trying to get Owner to work
     @NSManaged var pets: NSSet?
     @NSManaged var userID: User?
 
